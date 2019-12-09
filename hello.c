00be440fe9f7429f5a70ca0ccf79d26e3bb83d32
Kuzoto <Kuzoto@users.noreply.github.com>
2019-12-09 19:14:50 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- hello.c
+++ hello.c
@@ -3,6 +3,6 @@
 
 int main(void)
 {
-    string userName = get_string("What is your name?\n");
-    printf("hello, %s\n", userName);
+    string userName = get_string("What is your name?\n"); //Prompts user for name
+    printf("hello, %s\n", userName); //Receives input for string userName and prints it along with a hello
 }
