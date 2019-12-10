14c152dbc86d3d5a98b0c7a9a8e557d78b208497
Kuzoto <Kuzoto@users.noreply.github.com>
2019-12-10 17:20:54 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -4,7 +4,7 @@ void pyramid(void) {
     int h = get_int("Please Input Height: "); //Prompts user for pyramid height and width
     int n = h; //keeps track of number of blanks to print per row
     int x; //keeps track of current space in row
-    for (int i = 0; i < h; i++) { //repeat row print until i reaches height
+    for (int i = 0; i <= h; i++) { //repeat row print until i reaches height
         n--; //decrease n by 1 each time a new row starts
         for (x = 0; x <= h; x++) { //repeat column print per row until x reaches height
             if (x > n) { //print # while x is > than n
