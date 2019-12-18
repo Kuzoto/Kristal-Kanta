1ef8b3ac9b29e8d2df39268eb43235f910cca12a
Kuzoto <Kuzoto@users.noreply.github.com>
2019-12-18 22:28:17 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -3,70 +3,82 @@
 #include <string.h>
 #include <math.h>
 
-float qCoin = 25;
-float dCoin = 10;
-float nCoin = 5;
-float pCoin = 1;
-int qAmt = 0;
-int dAmt = 0;
-int nAmt = 0;
-int pAmt = 0;
-float change;
-int coinCount;
-void changeCoins(void)
+float qCoin = 25; //Quarter value
+float dCoin = 10; //Dime value
+float nCoin = 5; //Nickel value
+float pCoin = 1; //Penny value
+int qAmt = 0; //Quarter amount
+int dAmt = 0; //Dime amount
+int nAmt = 0; //Nickel amount
+int pAmt = 0; //Penny amount
+float change; //Change owed input storage
+int coinCount; //Total Amount of Coins
+void changeCoins(void) //See how many of each coin and total amount of coins you would need to give to the customer
 {
-    for(; ;)
+    for (; ;)
     {
-        change = get_float("Change Owed: ");
-        if (change < 0)
+        change = get_float("Change Owed: "); //Prompt for change owed input
+        if (change < 0) //Check if input is valid
         {
             continue;
-        } else {
+        } 
+        else 
+        {
             break;
         }
     }
-    int coins = round(change * 100);
-    for(; ;) {
-        if (coins >= qCoin)
+    int coins = round(change * 100); //Change float to an int
+    for (; ;)
+    {
+        if (coins >= qCoin) // check if coins is greater than or equal to a Quarter
         {
-            qAmt++;
-            coins = coins - qCoin;
+            qAmt++; //Increment quarter amount
+            coins = coins - qCoin; //subtract quarter value for change owed
             if (coins <= 0) 
             {
                 break;
             }
-        } else if (coins >= dCoin && coins < qCoin) {
-            dAmt++;
-            coins = coins - dCoin;
+        } 
+        else if (coins >= dCoin && coins < qCoin) //check if change owed is less than a quarter and greater than or equal to a dime
+        {
+            dAmt++; //increment dime amount
+            coins = coins - dCoin; //subtract change owed by dime value
             if (coins <= 0) 
             {
                 break;
             }
-        } else if (coins >= nCoin && coins < dCoin) {
-            nAmt++;
-            coins = coins - nCoin;
+        } 
+        else if (coins >= nCoin && coins < dCoin) //check if change owed is less than a dime and greater than or equal to a nickel
+        {
+            nAmt++; //increment nickel amount
+            coins = coins - nCoin; //subtract change owed by nickel value
             if (change <= 0) 
             {
                 break;
             }
-        } else if (coins >= pCoin && coins < nCoin) {
-            pAmt++;
-            coins = coins - pCoin;
-           if (coins <= 0) 
+        } 
+        else if (coins >= pCoin && coins < nCoin) //check if change owed is less than a nickel and greater than or equal to a penny
+        {
+            pAmt++; //increment penny amount
+            coins = coins - pCoin; //Subtract change owed by penny value
+            if (coins <= 0) //check if change owed equals 0
             {
                 break;
             }
-        } else {
+        } 
+        else
+        {
             break;
         }
     }
-    if (coins == 0) {
-        coinCount = qAmt + dAmt + nAmt + pAmt;
-        printf("Total Coins: %i\n", coinCount);
-        printf("Quarters: %i\nDimes: %i\nNickels: %i\nPennies: %i\n", qAmt, dAmt, nAmt, pAmt);
+    if (coins == 0)
+    {
+        coinCount = qAmt + dAmt + nAmt + pAmt; //Get coin total
+        printf("Total Coins: %i\n", coinCount); //Print coin total
+        printf("Quarters: %i\nDimes: %i\nNickels: %i\nPennies: %i\n", qAmt, dAmt, nAmt, pAmt); //Print individual coin amounts
     }
 }
 int main(void)
 {
-    changeCoins();
+    changeCoins(); //run function to find coin amount
 }
