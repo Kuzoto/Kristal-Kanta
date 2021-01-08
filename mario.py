ec9de94380ba0055abd0db5164259cfbab4b112f
Kuzoto <Kuzoto@users.noreply.github.com>
2021-01-08 17:38:12 +0000

    automated commit by check50 [check50=True]

---
+++ mario.py
@@ -0,0 +1,32 @@
+def main():
+    mario()
+
+def get_positive_int():
+    while True:
+        n = int(input("Positive Integer: "))
+        if n > 0:
+            break
+    return n
+        
+def mario():
+    x = get_positive_int()
+    s = x
+    for i in range(x):
+        for i in range(x):
+            if i < s-1:
+                print(" ", end="")
+            else:
+                print("#", end="")
+        print(" ", end="")
+        for i in range(x):
+            if i >= x-s+1:
+                print(" ", end="")
+            else:
+                print("#", end="")
+        print("")
+        s -= 1
+
+
+
+
+main()
