76ce68e31333d86e0fb41cc6473fc7bab7eee9f3
Kuzoto <Kuzoto@users.noreply.github.com>
2021-01-08 17:34:05 +0000

    automated commit by submit50

--- hello.py
+++ hello.py
@@ -1,14 +1,11 @@
 import cs50
 
-name = cs50.get_string("What is your name?\n")
-if str(name):
-    print("hello, " + name)
-else:
-    if str(name):
-        print("hello, " + name)
-    else:
-        name = cs50.get_string("Please enter a string\n")
-        if str(name):
-            print("hello, " + name)
-        else:
-            print("Error: string not entered")
+name = cs50.get_string("What is your name?\n")  # gets name from user
+if str(name):  # checks if input was a string/not null
+    print("hello, " + name)  # print hello, input
+else:  # if null/not string reprompt user 
+    name = cs50.get_string("Please enter a string\n")  # request string from user
+    if str(name):  # checks if input was a string/not null
+        print("hello, " + name)  # print hello, input
+    else:  # if null/not string return error
+        print("Error: string not entered")  # print error if they fail to enter a string twice
