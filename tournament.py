4bd78ddc7f65a906b85c7b5a124a9888a08ff395
Kuzoto <Kuzoto@users.noreply.github.com>
2021-01-12 22:26:11 +0000

    automated commit by check50 [check50=True]

--- tournament.py
+++ tournament.py
@@ -34,8 +34,8 @@ def main():
 
 def simulate_game(team1, team2):
     """Simulate a game. Return True if team1 wins, False otherwise."""
-    rating1 = float(team1["rating"])
-    rating2 = float(team2["rating"])
+    rating1 = int(team1["rating"])
+    rating2 = int(team2["rating"])
     probability = 1 / (1 + 10 ** ((rating2 - rating1) / 600))
     return random.random() < probability
 
